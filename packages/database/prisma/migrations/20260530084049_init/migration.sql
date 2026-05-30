f42baa1703372ee5e3f8154cb087e93b1e1094e2
yuriivintoniak <yurivintonyak@gmail.com>
2026-05-30 13:05:58 +0300

    chore: initialize prisma and database setup

--- packages/database/prisma/migrations/20260530084049_init/migration.sql
+++ packages/database/prisma/migrations/20260530084049_init/migration.sql
@@ -1,19 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
-
--- DropTable
-DROP TABLE "Post";
-
--- DropTable
-DROP TABLE "User";
-
 -- CreateTable
 CREATE TABLE "user" (
     "id" TEXT NOT NULL,
