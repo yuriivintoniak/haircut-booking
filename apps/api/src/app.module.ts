8b232a54ab837afc1413684a2916d1978583b505
yuriivintoniak <yurivintonyak@gmail.com>
2026-07-04 17:10:12 +0300

    feat: add booking model to Prisma schema

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,9 +4,10 @@ import { LinksModule } from './links/links.module';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
-  imports: [LinksModule],
+  imports: [LinksModule, BookingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
